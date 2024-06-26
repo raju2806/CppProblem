ea4bc53ecb58c5c80bbd46b9cfe318c313cb62a2
raju2806 <raju7654030601@gmail.com>
2024-06-26 17:53:57 +0530

    new

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,14 +4,16 @@ using namespace std;
 int main()
 
 {
-    int n;
+    int n,rem=0,sum=1;
     cout<<"Enter no to check palindrome: ";
     cin>>n;
     int temp=n;
     while(n>0)
     {
-        
+        rem =n%10;
+        sum=rem*10+sum;
+        n=n/10;
     }
 
-
+    return 0;
 }
