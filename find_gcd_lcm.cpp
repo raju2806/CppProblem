49b57daacb4fd68134727efcd77ff38b1599bd8c
raju2806 <raju7654030601@gmail.com>
2024-07-02 02:56:08 +0530

    find Lcm and Hcf

--- find_gcd_lcm.cpp
+++ find_gcd_lcm.cpp
@@ -3,17 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n1,n2,Hcf,Lcm;
+    long long n1,n2,Hcf,Lcm;
     cout<<"Enter first no and second no: ";
     cin>>n1>>n2;
     if(n1>n2) {swap(n1,n2);}
     cout<<n1<<" "<<n2<<endl;
-    for(int i=0; i<n1/2; i++)
+   
+    for(int j=1; j<=n1; j++)
     {
-        for(int j=1; j<n1; j++)
-        {
-            if(n1%j==0  && n2%j==0){Hcf=j;}
-        }
+        if(n1%j==0  && n2%j==0){Hcf=j;}
     }
     cout<<"Gcd of "<<n1 <<" and "<< n2<<" = " <<Hcf<<endl;
     Lcm=((n1*n2)/Hcf);
